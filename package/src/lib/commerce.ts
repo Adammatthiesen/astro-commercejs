f535cbf70dcb7ac4d5fbd89652d5dae33f0368d6
Adam Matthiesen <amatthiesen@outlook.com>
2024-02-08 18:21:52 -0800

    fix spelling error

--- package/src/lib/commerce.ts
+++ package/src/lib/commerce.ts
@@ -4,15 +4,15 @@ import { loadEnv } from 'vite';
 const {PUBLIC_COMMERCEJS_KEY} = loadEnv('all',process.cwd(),'PUBLIC_COMMERCEJS_');
 
 const publicKey = PUBLIC_COMMERCEJS_KEY;
-const cpublicKey = publicKey?publicKey:"";
+const cpublicKey = publicKey ? publicKey : "";
 const isDevENV = process.env.NODE_ENV === 'development';
 
 export const commerce = new Commerce(cpublicKey,isDevENV)
 
 export async function AstroCommerceJS() {
     const merchant = await commerce.merchants.about();
-    const { data: catagories } = await commerce.categories.list();
+    const { data: categories } = await commerce.categories.list();
     const { data: products } = await commerce.products.list();
 
-    return { merchant, catagories, products};
+    return { merchant, categories, products};
 }
